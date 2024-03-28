3911c8bd1288ec3cf423508f252d29c769bb83cf
Tommy Xiang <tommyx058@gmail.com>
2024-03-27 19:19:00 -0700

    update

--- music_copilot_helper.py
+++ music_copilot_helper.py
@@ -1,3 +1,5 @@
+TEMPERATURE = 0.5
+
 try:
     from openai import OpenAI
     import json
@@ -8,13 +10,23 @@ try:
     message_json = json.loads(raw_message)
 
     client = OpenAI(api_key=message_json["api_key"])
-    response = client.chat.completions.create(
-        model=message_json["model"],
-        messages=[{"role": "user", "content": message_json["prompt"]}],
-        temperature=0.25,
-    )
-    print("SUCCESS")
-    print(response.choices[0].message.content)
+    if message_json["mode"] == "chat":
+        response = client.chat.completions.create(
+            model=message_json["model"],
+            messages=[{"role": "user", "content": message_json["prompt"]}],
+            temperature=TEMPERATURE,
+        )
+        print("SUCCESS")
+        print(response.choices[0].message.content)
+
+    elif message_json["mode"] == "completion":
+        response = client.completions.create(
+            model=message_json["model"],
+            prompt=message_json["prompt"],
+            temperature=TEMPERATURE,
+        )
+        print("SUCCESS")
+        print(response.choices[0].text)
 
 except Exception as e:
     print("ERROR")
